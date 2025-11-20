ff9e65fa55306ecd860f91ec5ac9672f496237c2
kaifahmedsiddiqui-47 <kaifahmedsiddiqui@gmail.com>
2025-11-20 15:57:24 +0530

    Create week2.py

---
+++ week2.py
@@ -0,0 +1,21 @@
+SUB1=int(input("ENTER YOUR MARKS IN 1ST SUBJECT:"))
+if(SUB1>=85):
+    print("A grade, very well")
+elif(SUB1>=75):
+    print("B grade, good!")
+else:
+    print("c grade, work hard ")
+SUB2=int(input("ENTER YOUR MARKS IN 2ND SUBJECT:"))
+if(SUB2>=85):
+    print("A grade, very well")
+elif(SUB2>=75):
+    print("B grade, good!")
+else:
+    print("c grade, work hard ")
+SUB3=int(input("ENTER YOUR MARKS IN 3RD SUBJECT:"))
+if(SUB3>=85):
+    print("A grade, very well")
+elif(SUB3>=75):
+    print("B grade, good!")
+else:
+    print("c grade, work hard ")
